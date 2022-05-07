2bba56c842f171f9fce9d5fffee03cb53678e9da
Mwni <manuel@tigmar.de>
2022-05-07 03:20:44 +0200

    added XFL scientific notation support

--- xfl/conversion.js
+++ xfl/conversion.js
@@ -7,22 +7,28 @@ export function fromString(str){
 		return XFL({mantissa: 0n, exponent: 0n})
 
 	let mantissa
-	let exponent
+	let exponent = 0n
 	let negative = false
 	let point = str.indexOf('.')
+	let e = str.lastIndexOf('e')
 
 	if(str.charAt(0) === '-'){
 		str = str.slice(1)
 		negative = true
 		point--
+		e--
+	}
+
+	if(e >= 0){
+		exponent += BigInt(str.slice(e + 1))
+		str = str.slice(0, e)
 	}
 
 	if(point > 0){
 		mantissa = BigInt(str.slice(0, point) + str.slice(point + 1))
-		exponent = BigInt(point - str.length + 1)
+		exponent += BigInt(point - str.length + 1)
 	}else{
 		mantissa = BigInt(str)
-		exponent = BigInt(0)
 	}
 
 	if(negative)
@@ -35,8 +41,14 @@ export function toString(xfl){
 	if(xfl.mantissa === 0n)
 		return '0'
 
+	let prefix = ''
 	let str = xfl.mantissa.toString()
 	let point = Number(xfl.exponent - minExponent - maxExponent)
+
+	if(xfl.mantissa < 0n){
+		prefix = '-'
+		str = str.slice(1)
+	}
 
 	if(point <= 0){
 		str = `0.`.padEnd(2 - point, '0') + str
@@ -56,7 +68,7 @@ export function toString(xfl){
 	if(str.charAt(cap) === '.')
 		cap--
 
-	return str.slice(0, cap + 1)
+	return prefix + str.slice(0, cap + 1)
 }
 
 
