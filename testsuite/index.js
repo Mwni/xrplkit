1bcbcaef4b3194e86ae4fe639090485112eca598
Mwni <manuel@tigmar.de>
2022-05-07 19:14:24 +0200

    added testsuite package

---
+++ testsuite/index.js
@@ -0,0 +1,14 @@
+
+
+/*
+{
+    "account": {
+        "xAddress": "TVTLKxtt1WWSQJcaEZzbmjT8EfzzUvbBY1MBp5B6HW8x53G",
+        "secret": "sha2KGnvLFz27ZAkt2JkdwrpYugfP",
+        "classicAddress": "rLfU3d3jA6DfKciG5oRcNbziMEaWdzcGWn",
+        "address": "rLfU3d3jA6DfKciG5oRcNbziMEaWdzcGWn"
+    },
+    "amount": 1000,
+    "balance": 1000
+}
+*/
