903b7cfced09402a7a25daa3eff2a23521ace37e
Mwni <manuel@tigmar.de>
2022-05-23 23:42:10 +0200

    added XFL sort function

---
+++ xfl/extras.js
@@ -0,0 +1,22 @@
+import { XFL, eq, lt } from './index.js'
+
+export function sort(array, key){
+	if(array){
+		return array
+			.map(item => [item, typeof key === 'function' ? key(item) : item[key]])
+			.sort((a, b) => compare(a[1], b[1]))
+			.map(([item]) => item)
+
+	}else{
+		return array
+			.slice()
+			.sort(compare)
+	}
+}
+
+function compare(a, b){
+	if(eq(a, b))
+		return 0
+
+	return lt(a, b) ? -1 : 1
+}
