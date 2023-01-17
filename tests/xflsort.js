aaea1131d6a019c4ce2447b5b7da9e6b3440c447
Mwni <manuel@tigmar.de>
2023-01-17 18:57:54 +0100

    test xfl sort safety

--- tests/xflsort.js
+++ tests/xflsort.js
@@ -1,14 +1,30 @@
 import { toBigInt, toSortSafeBigInt } from '@xrplkit/xfl'
 
+function dec2bin(dec) {
+	return (dec >>> 0).toString(2)
+}
+
+console.log(dec2bin(Number.MAX_SAFE_INTEGER - 1053200000))
+console.log(dec2bin(~((Number.MAX_SAFE_INTEGER - 1053200000) * -1)))
+process.exit()
+
 let array = Array(10).fill(0)
 	.map(() => ({ value: Math.random() * 10 - 5 }))
 
 let negShift = array
-	.map(({ value }) => ({ value, sort: toBigInt(value) << 1n }))
+	.map(({ value }) => {
+		let sort = toBigInt(value)
+
+		sort = sort << 1n
+		
+
+		return { value, sort }
+	})
 
 let negXor = array
 	.map(({ value }) => ({ value, sort: toSortSafeBigInt(value) }))
 
+	console.log(negShift)
 
 console.log(
 	'using left shift:\n',
