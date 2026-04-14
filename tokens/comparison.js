20a7ae8226c146f4ba83bd9cc7c045dd35e3d6d4
Raj Patel <rajp@ripple.com>
2026-04-13 20:33:40 -0400

    handle MPTs in amount and comparison

--- tokens/comparison.js
+++ tokens/comparison.js
@@ -3,19 +3,26 @@ import { currencyUTF8ToHex } from './currency.js'
 export function isSameToken(a, b){
 	if(typeof a === 'string')
 		a = {currency: 'XRP'}
+	else if(a.mpt_issuance_id)
+		a = { mpt_issuance_id: a.mpt_issuance_id }
 	else
 		a = {
-			currency: currencyUTF8ToHex(a.currency), 
+			currency: currencyUTF8ToHex(a.currency),
 			issuer: a.issuer || a.account
 		}
 
 	if(typeof b === 'string')
 		b = {currency: 'XRP'}
+	else if(b.mpt_issuance_id)
+		b = { mpt_issuance_id: b.mpt_issuance_id }
 	else
 		b = {
-			currency: currencyUTF8ToHex(b.currency), 
+			currency: currencyUTF8ToHex(b.currency),
 			issuer: b.issuer || a.account
 		}
+
+	if(a.mpt_issuance_id || b.mpt_issuance_id)
+		return a.mpt_issuance_id === b.mpt_issuance_id
 
 	return true
 		&& a.currency === b.currency
